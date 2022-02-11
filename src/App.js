ba681ea30709b7feb89c9057e8da29a60a21e046
Xavier Brassard <xtartaglia@gmail.com>
2022-02-11 03:20:41 +0100

    lol

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
-import Matter, { Body, Composite } from 'matter-js'
+import Matter, { Body, Composite, Bodies } from 'matter-js'
 import { useEffect, useRef, useState } from 'react'
 import Domanda from './Domanda'
 import doggo from './doggo.png'
 import audio from './i-ruv-you-1.mp3'
 import domande from './domande.json'
+import { fabric } from 'fabric'
+
+function createImage(string, w, h) {
+
+  var canvas = new fabric.Canvas()
+
+  canvas.width = w
+  canvas.height = h
+  
+
+  var text = new fabric.Textbox(string,{
+      width:w,
+      fontSize:15,
+      textAlign:'center',
+      fontFamily: 'Ubuntu',
+      fill: '#fff',
+      stroke: '#000',
+      strokeWidth: 4,
+      paintFirst: 'stroke', // stroke behind fill
+  })
+
+  canvas.add(text)
+  canvas.centerObject(text)
+
+  return canvas.toDataURL("image/png");
+}
+
+function getAnswers(q,screenHeight,screenWidth,playerPos,groundHeight) {
+  let r = Object.keys(q)
+  let rgraph = Composite.create()
+  console.log(r)
+
+  for (let i=0;i<r.length-1;i++) {
+    var sprite = createImage(domande["domanda0"]["r"+i].r.toUpperCase(),200,(screenHeight-groundHeight)/4.5*0.6)
+    console.log(sprite)
+    var ans = Bodies.rectangle(playerPos+screenWidth/4,(2*i+1)*(screenHeight-groundHeight)/9+(screenHeight-groundHeight)/18,200,(screenHeight-groundHeight)/4.5*0.6,{isStatic:true, isSensor:true, render:{sprite:{texture:sprite}}})
+    ans.collisionFilter = {
+      'group': -1,
+      'category': 2,
+      'mask': 0,
+    }
+    Composite.add(rgraph, [ans])
+  }
+
+  return rgraph;
+}
 
 
 export default function App(props) {
@@ -11,9 +57,9 @@ export default function App(props) {
   let resize = false
   let gameOver = false
   let j = 1;
-  let clean = false;
-  let freeVar = "domanda2"
+  let clean = false
   var punteggio = 0
+
 
   useEffect(()=>{
     var Engine = Matter.Engine,
@@ -40,10 +86,10 @@ export default function App(props) {
       }
     })
 
-    var player = Bodies.rectangle(100, window.innerHeight/2, 50, 37, {chamfer: {radius: 15}, render:{sprite:{texture:doggo}}})
+    var player = Bodies.rectangle(window.innerWidth/4, window.innerHeight/2, 50, 37, {chamfer: {radius: 15}, render:{sprite:{texture:doggo}}})
     Body.setMass(player, 20)
 
-    var camera = Bodies.rectangle(player.position.x, player.position.y,1,1, {isSensor:true, isStatic:true, render:{visible:false}})
+    var camera = Bodies.rectangle(player.position.x+render.options.width/4, player.position.y,1,1, {isSensor:true, isStatic:true, render:{visible:false}})
     camera.collisionFilter = {
       'group': -1,
       'category': 2,
@@ -55,7 +101,18 @@ export default function App(props) {
         engine.gravity.y = 0.05
         domanda = Domanda({screenHeight:render.options.height,groundHeight:render.options.height/50, x:render.options.width/2+300, q:domande["domanda0"]})
         Composite.add(engine.world, [domanda])
-        document.body.style.animationPlayState = "running"
+        document.body.style.animationPlayState = "running"  
+
+        var qr = Composite.create()
+        var sprite = createImage(domande["domanda0"].d.toUpperCase(),300,100)
+        console.log(sprite)
+        var q = Bodies.rectangle(player.position.x,player.position.y-render.options.height/4,300,100, {isSensor:true, isStatic:true, render:{sprite:{texture:sprite}}})
+        Composite.add(qr, [q])
+        Composite.add(engine.world, [qr])
+        setTimeout(()=>{
+          Composite.add(qr,[getAnswers(domande["domanda0"],render.options.height,render.options.width,player.position.x,render.options.height/50)])
+        },1000)
+
       }
       console.log("click")
       if (!gameOver) {
@@ -106,27 +163,8 @@ export default function App(props) {
     
 
     Events.on(engine, "afterUpdate", ()=>{
-      if (!gameOver) {
-        Body.applyForce(player, {x:player.position.x, y:player.position.y}, {x:0.001, y:0})
-        Body.setPosition(camera, {x:player.position.x,y:render.options.height/2})
-        Body.setPosition(ground, {x:player.position.x,y:ground.position.y})
-        Render.lookAt(render, camera,  {
-        x: window.innerWidth/2,
-        y: window.innerHeight/2
-      });
-      }
-
-      else {
-        //Engine.clear(engine)
-        //console.log("game over")
-      }
-
-      if (engine.gravity !== 0 && typeof domanda.bodies[0].position.x !== 'undefined' && (player.position.x-Composite.allBodies(engine.world)[Composite.allBodies(engine.world).length-1].position.x)>10 && !clean) {
-        console.log(Composite.allBodies(engine.world)[Composite.allBodies(engine.world).length-1])
-        clean = true
-
-        handleChange(Composite.allBodies(engine.world)[Composite.allBodies(engine.world).length-1])
-        j++
+      if (typeof domanda !== "undefined" && !gameOver) {
+        Composite.translate(domanda, {x:-5, y:0})
       }
     })
 
@@ -143,7 +181,7 @@ export default function App(props) {
 
       else {
         punteggio++
-        console(punteggio)
+        console.log(punteggio)
       }
 
       // change object colours to show those in an active collision (e.g. resting contact)
--- src/Domanda.js
+++ src/Domanda.js
@@ -2,47 +2,8 @@ import { useEffect, useRef } from 'react'
 import { Engine, Bodies, World, Composite, Body } from 'matter-js'
 import { fabric } from 'fabric';
 
-
-function createCanvas(width, height, set2dTransform = true, id) {
-    const ratio = Math.ceil(window.devicePixelRatio);
-    const canvas = document.createElement('canvas');
-    canvas.width = width * ratio;
-    canvas.height = height * ratio;
-    canvas.style.width = `${width}px`;
-    canvas.style.height = `${height}px`;
-    canvas.id = id;
-    if (set2dTransform) {
-      canvas.getContext('2d').setTransform(ratio, 0, 0, ratio, 0, 0);
-    }
-    return canvas;
-}
-
-function createImage(string, w, h) {
-
-    var canvas = new fabric.Canvas()
-
-    canvas.width = w
-    canvas.height = h
-    
-
-    var text = new fabric.Textbox(string,{
-        width:w,
-        fontSize:15,
-        textAlign:'center',
-        fill: '#fff',
-        stroke: '#000',
-        strokeWidth: 4,
-        paintFirst: 'stroke', // stroke behind fill
-    })
-
-    canvas.add(text)
-    canvas.centerObject(text)
-
-    return canvas.toDataURL("image/jpeg",1.0);
-}
-
 export default function Domanda (props) {
-  const { screenHeight, groundHeight, x, q } = props
+  const { screenHeight, groundHeight, x, q, player } = props
   const domanda = Composite.create()
 
   const altezzaA = (screenHeight-groundHeight)/4.5*0.4
@@ -50,15 +11,6 @@ export default function Domanda (props) {
   const altezzaC = (screenHeight-groundHeight)/9
   const larghezza = 200
 
-
-  var question = Bodies.rectangle(x, (screenHeight-groundHeight)/2, 300, 100, {isStatic:true, isSensor:true, render:{sprite:{texture:createImage(q.d.toUpperCase(),400,100)}}})
-  question.collisionFilter = {
-    'group': -1,
-    'category': 2,
-    'mask': 0
-}
-
-  Composite.add(domanda, question)
   for (let i=0;i<9;i++) {
 
     if (i%2===0 && i !==8 && i !== 0) {
@@ -74,9 +26,7 @@ export default function Domanda (props) {
     }
 
     else {
-        var sprite = createImage(q["r"+(i-1)/2].r.toUpperCase(),larghezza,altezzaB)
-        var box = Bodies.rectangle(x+500,i*(screenHeight-groundHeight)/9+(screenHeight-groundHeight)/18,larghezza,altezzaB, {isStatic:true, isSensor:true})
-        box.render.sprite.texture = sprite;
+        var box = Bodies.rectangle(x+500,i*(screenHeight-groundHeight)/9+(screenHeight-groundHeight)/18,larghezza,altezzaB, {isStatic:true, isSensor:true, render:{visible:false}})
 
         if (!q["r"+(i-1)/2].giusto) {
             box.collisionFilter = {
@@ -84,6 +34,10 @@ export default function Domanda (props) {
                 'category': 2,
                 'mask': 0,
               };
+        }
+
+        else {
+            console.log("giusto!")
         }
     }
     Composite.add(domanda, box)
