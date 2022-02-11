cdb4878cf4185385552fc1a9f6c0b31a109e6c29
Xavier Brassard <xtartaglia@gmail.com>
2022-02-11 05:34:32 +0100

    lol

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ export default function App(props) {
 
 
   const [scale, setScale] = useState(window.innerHeight/746)
-  const [scaleX, setScaleX] = useState(1*window.devicePixelRatio)
+  const [scaleX, setScaleX] = useState(1/window.devicePixelRatio)
 
   console.log(scale)
   alert("Here is your Pixel ratio: "+window.devicePixelRatio)
