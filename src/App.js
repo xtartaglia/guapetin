a71fa9f3fe75a7297b31625e0c9a2ff4c8e285b9
Xavier Brassard <xtartaglia@gmail.com>
2022-03-02 04:45:14 +0100

    loool

--- src/App.js
+++ src/App.js
@@ -681,7 +681,7 @@ export default function App(props) {
       }
 
       player = Bodies.rectangle(window.screen.width / 3, window.screen.height / 2, 50 * scale, 37 * scale, { chamfer: { radius: 15 }, render: { sprite: { texture: doggo, xScale: scale, yScale: scale } } })
-      Body.setMass(player, 20)
+      Body.setMass(player, 20*scale)
       
 
 
