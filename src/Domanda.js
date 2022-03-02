9e346ac71e5f86e4ff0322ba9581fc2942d1bb50
Xavier Brassard <xtartaglia@gmail.com>
2022-03-02 03:52:34 +0100

    loool

--- src/Domanda.js
+++ src/Domanda.js
@@ -17,19 +17,19 @@ export default function Domanda (props) {
   for (let i=0;i<(2*r.length-1);i++) {
 
     if (i%2===0 && i !==(2*r.length-2) && i !== 0) {
-        box = Bodies.rectangle(x+500,i*(screenHeight-groundHeight)/(2*r.length-1)+(screenHeight-groundHeight)/(2*r.length-1)/2,larghezza-10,altezzaA, {isStatic:true, label:'box'+i, chamfer: {radius:altezzaA/2},render:{overflow:'hidden',sprite:{texture:nuvola,xScale:1.2,yScale:altezzaA/100*1.6}}})
+        box = Bodies.rectangle(x,i*(screenHeight-groundHeight)/(2*r.length-1)+(screenHeight-groundHeight)/(2*r.length-1)/2,larghezza-10,altezzaA, {isStatic:true, label:'box'+i, chamfer: {radius:altezzaA/2},render:{overflow:'hidden',sprite:{texture:nuvola,xScale:1.2,yScale:altezzaA/100*1.6}}})
     }
 
     else if (i === 0) {
-        box = Bodies.rectangle(x+500,i*(screenHeight-groundHeight)/(2*r.length-1)+(screenHeight-groundHeight)/(2*r.length-1)/2,larghezza-10,altezzaC, {isStatic:true, label:'box'+i, chamfer: {radius:altezzaC/2},render:{overflow:'hidden',sprite:{texture:nuvola,xScale:1.2,yScale:altezzaC/100*1.6}}})
+        box = Bodies.rectangle(x,i*(screenHeight-groundHeight)/(2*r.length-1)+(screenHeight-groundHeight)/(2*r.length-1)/2,larghezza-10,altezzaC, {isStatic:true, label:'box'+i, chamfer: {radius:altezzaC/2},render:{overflow:'hidden',sprite:{texture:nuvola,xScale:1.2,yScale:altezzaC/100*1.6}}})
     }
 
     else if (i === (2*r.length-2)) {
-        box = Bodies.rectangle(x+500,screenHeight-screenHeight/(2*r.length-1)/2,larghezza,altezzaD, {isStatic:true, chamfer: {radius: 15}, label:'box'+i, render:{overflow:'hidden',sprite:{texture:groundstuck,xScale:1,yScale:altezzaD/150}}})
+        box = Bodies.rectangle(x,screenHeight-screenHeight/(2*r.length-1)/2,larghezza,altezzaD, {isStatic:true, chamfer: {radius: 15}, label:'box'+i, render:{overflow:'hidden',sprite:{texture:groundstuck,xScale:1,yScale:altezzaD/150}}})
     }
 
     else {
-        box = Bodies.rectangle(x+500,i*i*(screenHeight-groundHeight)/(2*r.length-1)+(screenHeight-groundHeight)/(2*r.length-1)/2,larghezza,altezzaB, {isStatic:true, isSensor:true, label:'box'+i, render:{visible:false}})
+        box = Bodies.rectangle(x,i*i*(screenHeight-groundHeight)/(2*r.length-1)+(screenHeight-groundHeight)/(2*r.length-1)/2,larghezza,altezzaB, {isStatic:true, isSensor:true, label:'box'+i, render:{visible:false}})
         if (!q["r"+(i-1)/2].giusto) {
             box.collisionFilter = {
                 'group': -1,
