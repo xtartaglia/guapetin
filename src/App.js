471c1b2ac3d6439e9f43a187ce518c1f9bcb897e
Xavier Brassard <xtartaglia@gmail.com>
2022-03-02 04:42:04 +0100

    loool

--- src/App.js
+++ src/App.js
@@ -397,7 +397,7 @@ export default function App(props) {
               if (result.value === true) {
                 document.body.requestFullscreen()
                 mov = -2
-                engine.gravity.y = 0.2 * scale * scaleX
+                engine.gravity.y = 0.1
               }
 
               else {
@@ -433,7 +433,7 @@ export default function App(props) {
       if (justStarted && !gameOver && fullscreen) {
         justStarted = false
 
-        engine.gravity.y = 0.2 * scale * scaleX
+        engine.gravity.y = 0.1
         domanda = Domanda({ screenHeight: window.screen.height, groundHeight: window.screen.height / 12.5, x: player.position.x + 2000, q: domande["domanda0"], scale: scale })
         Composite.add(engine.world, [domanda])
         document.body.style.animationPlayState = "running"
@@ -644,7 +644,7 @@ export default function App(props) {
         if (volume !== 0) {
           sound.play()
         }
-        Body.applyForce(player, { x: player.position.x, y: player.position.y }, { x: 0, y: -0.2 * scale * scaleX })
+        Body.applyForce(player, { x: player.position.x, y: player.position.y }, { x: 0, y: -0.1 })
       }
     }
 
@@ -667,7 +667,7 @@ export default function App(props) {
       } else {
 
         mov = -2
-        engine.gravity.y = 0.2 * scale *scaleX
+        engine.gravity.y = 0.1
       }
     }, false);
 
